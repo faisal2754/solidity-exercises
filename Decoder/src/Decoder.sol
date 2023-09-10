d7e1fb301a87868c7e887b08b9c40e302d4ae894
Faisal Saleem <saleemfs27@gmail.com>
2023-09-10 20:41:53 +0200

    Complete

--- Decoder/src/Decoder.sol
+++ Decoder/src/Decoder.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
+
 contract Decoder {
     /* This exercise assumes you know how abi decoding works.
         1. In the `decodeData` function below, write the logic that decodes a `bytes` data, based on the function parameters
@@ -10,6 +12,12 @@ contract Decoder {
 
     function decodeData(
         bytes memory _data
-    ) public pure returns (string memory, uint256) {}
-}
+    ) public pure returns (string memory, uint256) {
+        (string memory someStr, uint256 someUint) = abi.decode(
+            _data,
+            (string, uint256)
+        );
 
+        return (someStr, someUint);
+    }
+}
